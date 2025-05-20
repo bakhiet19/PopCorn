9cd076dac2f10fb2a3a945295e207d92edf9ceaf
bakhiet19 <bakhiet19>
2025-05-20 21:46:32 +0200

    Solve some Problems

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useMovies } from "./useMovies";
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "831333e7";
+// const KEY = "831333e7";
 
 export default function App() {
   const [query, setQuery] = useState("");
